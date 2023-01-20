22b6c8e58f2b37ee94eaba30eb9b612288c55698
TJ <tjdrzewiecki@gmail.com>
2023-01-19 22:09:06 -0500

    Added PDF library and declarations file for it since no @types exist

---
+++ declarations.d.ts
@@ -0,0 +1,1 @@
+declare module "@react-18-pdf/renderer"
---
+++ src/components/MyDocument.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Document } from '@react-18-pdf/renderer';
+
+function MyDocument() {
+    return (
+      <Document></Document>
+    );
+  }
+  
+  export default MyDocument;
