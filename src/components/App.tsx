6ae2101fde9baa493d74add9c0d82603daa9a2e7
TJ <tjdrzewiecki@gmail.com>
2023-01-20 20:45:39 -0500

    Basic form creation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [code, setCode] = useState("");
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [person, setPerson] = useState("");
+
+  const formSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setCode("");
+    setName("");
+    setPrice(0);
+    setPerson("");
+  };
+
   return (
     <main className="App">
       <header>Quote Generator</header>
-      <form>
-        <input placeholder="Enter Product Code" required />
-        <input placeholder="Enter Product Name (Optional)" />
-        <input placeholder="Enter Price / UOM" required/>
-        <input placeholder="Enter Your Name" required/>
+      <form onSubmit={(e) => formSubmit(e)}>
+        <input
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+          placeholder="Enter Product Code"
+          required
+        />
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter Product Name (Optional)"
+        />
+        <input
+          type="number"
+          value={price}
+          onChange={(e) => setPrice(parseFloat(e.target.value))}
+          placeholder="Enter Price / UOM"
+          required
+        />
+        <input
+          value={person}
+          onChange={(e) => setPerson(e.target.value)}
+          placeholder="Enter Your Name"
+          required
+        />
         <button>Submit</button>
       </form>
     </main>
